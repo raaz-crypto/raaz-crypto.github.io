944b53caf439f38fe1c3c65133b6badcfe61fbf8
Piyush P Kurur <ppk@cse.iitk.ac.in>
2019-01-05 15:22:57 +0530

    added the deploy script.

---
+++ deploy.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+# Temporarily store uncommited changes
+git stash
+
+# Verify correct branch
+git checkout develop
+
+# Build new files
+cabal new-build
+cabal new-exec site build
+
+# Get previous files
+git fetch --all
+git checkout -b master --track github/master
+
+# Overwrite existing files with new files
+rsync -a --ignore-times --filter='P _site/' --filter='P .travis.yml' --filter='P _cache/' --filter='P .git/' --filter='P .stack-work' --filter='P .gitignore' --delete-excluded _site/  .
+
+# Commit
+git add -A
+git commit --no-verify -m "Publish."
+
+# Push
+git push github master:master
+
+# Restoration
+git checkout develop
+git branch -D master
+git stash pop
